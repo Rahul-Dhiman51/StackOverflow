8562b1a605134eef9ab6e8a20be21a7a65fed10b
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2023-11-08 12:48:03 +0530

    Share URL updated

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -22,7 +22,7 @@ const QuestionsDetails = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const location = useLocation();
-	const url = 'http://localhost:3000'
+	const url = 'https://stack-overflow-rd.netlify.app'
 
 	const handlePostAns = (e, answerLength) => {
 		e.preventDefault();
